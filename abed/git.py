9db35b5b65b7bd67a69ab33f423568e7a048e634
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2015-05-05 15:43:37 +0200

    print git error messages when they occur

--- abed/git.py
+++ abed/git.py
@@ -12,36 +12,42 @@ def git_init():
     # init repo
     try:
         check_output(['git', 'init'])
-    except CalledProcessError:
-        error("Error executing 'git init'")
-        raise
+    except CalledProcessError as err:
+        error("Error executing 'git init'. Error message:")
+        print(err.output)
+        raise SystemExit
     # add conf
     try:
         check_output(['git', 'add', 'abed_conf.py'])
-    except CalledProcessError:
-        error("Error executing 'git add abed_conf.py'")
-        raise
+    except CalledProcessError as err:
+        error("Error executing 'git add abed_conf.py'. Error message:")
+        print(err.output)
+        raise SystemExit
     try:
         check_output(['git', 'commit' ,'-am', '"initialized abed skeleton"'])
-    except CalledProcessError:
-        error("Error executing initial commit")
-        raise
+    except CalledProcessError as err:
+        error("Error executing initial commit. Error message:")
+        print(err.output)
+        raise SystemExit
 
 def git_add_tbd():
     # add tbd
     try:
         check_output(['git', 'add', settings.TASK_FILE])
-    except CalledProcessError:
-        error("Error executing 'git add %s'" % settings.TASK_FILE)
-        raise
+    except CalledProcessError as err:
+        error("Error executing 'git add %s'. Error message:" % 
+                settings.TASK_FILE)
+        print(err.output)
+        raise SystemExit
 
 def git_commit_tbd():
     try:
         check_output(['git', 'commit', '-m',
-            '"automatic commit of TBD task file"', settings.TASK_FILE])
-    except CalledProcessError:
-        error("Error performing autocommit for TBD file")
-        raise
+            "'automatic commit of TBD task file'", settings.TASK_FILE])
+    except CalledProcessError as err:
+        error("Error performing autocommit for TBD file. Error message:")
+        print(err.output)
+        raise SystemExit
     info("Automatic TBD file commit")
 
 def git_ok():
@@ -50,6 +56,7 @@ def git_ok():
     except CalledProcessError as err:
         if err.returncode == 1:
             return False
-        error("Error performing 'git diff --exit-code'")
-        raise
+        error("Error performing 'git diff --exit-code'. Error message:")
+        print(err.output)
+        raise SystemExit
     return True
