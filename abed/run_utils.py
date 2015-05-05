577f24defe55b0e9b7fb466755b0365c0241accd
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2015-05-05 22:47:05 +0200

    properly find the latest subdir

--- abed/run_utils.py
+++ abed/run_utils.py
@@ -18,14 +18,14 @@ def get_output_dir(result_dir):
         mkdir(outdir)
         info("Created result output dir %s" % outdir)
         return outdir
-    latest = sorted(subdirs)[-1]
-    files = os.listdir(result_dir + '/' + latest)
+    latest = sorted(map(int, subdirs))[-1]
+    files = os.listdir(result_dir + '/' + str(latest))
     if len(files) >= settings.MAX_FILES_DIR:
-        outdir = '%s/%i' % (result_dir, int(latest) + 1)
+        outdir = '%s/%i' % (result_dir, latest + 1)
         mkdir(outdir)
         info("Created result output dir %s" % outdir)
     else:
-        outdir = '%s/%s' % (result_dir, latest)
+        outdir = '%s/%i' % (result_dir, latest)
     return outdir
 
 def write_output(output, hsh):
