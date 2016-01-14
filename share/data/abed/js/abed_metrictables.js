5b48f62caa50e84b53fbb561e4be65c88c4b1ae6
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2016-01-14 17:27:58 +0100

    make metrictables work for assess type experiments

--- share/data/abed/js/abed_metrictables.js
+++ share/data/abed/js/abed_metrictables.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function ($) {
-	var selectors = $("input[name=target], input[name=trainmetricname], input[name=testmetricname], input[name=type]");
+	var tmp = $("input[name=trainmetricname]");
+	var selectors;
+	if (tmp.length == 0) {
+		selectors = $("input[name=target], input[name=metricname], input[name=type]");
+	} else {
+		selectors = $("input[name=target], input[name=trainmetricname], input[name=testmetricname], input[name=type]");
+	}
 	ShowTables(selectors);
 	selectors.on("change", function(e) {
 		ShowTables(selectors);
