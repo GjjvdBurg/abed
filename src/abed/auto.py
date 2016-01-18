afd69700210f9b2e3539c54d05e82799c35efc10
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2016-01-18 14:34:15 +0100

    Skip over errors when getting logs

--- src/abed/auto.py
+++ src/abed/auto.py
@@ -46,7 +46,10 @@ def get_jobid_from_pbs():
 def get_jobid_from_logs(logpath=None):
     if logpath is None:
         logpath = '%s/releases/current/logs/' % settings.REMOTE_DIR
-    text = myfab.run("ls -1 %s" % logpath)
+    try:
+        text = myfab.run("ls -1 %s" % logpath)
+    except:
+        return None
     if not text:
         return None
     jobid = text.split('\n')[0].split('.')[-1][1:]
