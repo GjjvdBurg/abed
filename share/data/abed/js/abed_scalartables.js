c808f6954291fa2dca6d6445cd03a8a34a4486bf
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2016-05-26 22:31:57 +0200

    removed abed files on pages branch

--- share/data/abed/js/abed_scalartables.js
+++
@@ -1,8 +0,0 @@
-
-jQuery(document).ready(function ($) {
-	var selectors = $("input[name=target], input[name=type]");
-	ShowTables(selectors);
-	selectors.on("change", function(e) {
-		ShowTables(selectors);
-	})
-});
