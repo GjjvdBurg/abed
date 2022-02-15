84168add76271098bbef2105a4aa48b30251c355
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2022-02-15 13:24:20 +0000

    make error messages colorful

--- abed/io.py
+++ abed/io.py
@@ -4,22 +4,47 @@
 I/O stuff
 """
 
+import colorama
 import sys
 
 
+def color_string(msg, color=None, style=None):
+    if not hasattr(cprint, "init"):
+        cprint.init = False
+    if not cprint.init:
+        colorama.init()
+        cprint.init = True
+
+    colors = {
+        "red": colorama.Fore.RED,
+        "green": colorama.Fore.GREEN,
+        "cyan": colorama.Fore.CYAN,
+        "yellow": colorama.Fore.YELLOW,
+        "magenta": colorama.Fore.MAGENTA,
+        None: "",
+    }
+    styles = {
+        "bright": colorama.Style.BRIGHT,
+        "dim": colorama.Style.DIM,
+        None: "",
+    }
+    pre = colors[color] + styles[style]
+    post = colorama.Style.RESET_ALL
+    return f"{pre}{msg}{post}"
+
+
+def cprint(msg, color=None, style=None, file=sys.stdout):
+    print(color_string(msg, color=color, style=style))
+    file.flush()
+
+
 def info(txt):
-    message = "%s" % txt
-    print(message, file=sys.stdout)
-    sys.stdout.flush()
+    cprint(txt)
 
 
 def error(txt):
-    message = "Error: %s" % (txt)
-    print(message, file=sys.stderr)
-    sys.stderr.flush()
+    cprint(f"ERROR: {txt}", color="red", file=sys.stderr)
 
 
 def warning(txt):
-    message = "Warning: %s" % (txt)
-    print(message, file=sys.stderr)
-    sys.stderr.flush()
+    cprint(f"WARN: {txt}", color="yellow", file=sys.stderr)
